24479331c4eeaef8e9adb81e059404d9ebd6a303
Konstancja Glinka <timey-wimey-kid@yandex.ru>
2022-08-03 20:33:07 +0300

    Еще одна попытка запушить

---
+++ src/test/java/setup/SetUpChrome.java
@@ -0,0 +1,16 @@
+package setup;
+
+import org.junit.Before;
+
+public class SetUpChrome extends SetUpDriver {
+
+    @Before
+    public void chromeTestSetup() {
+        chromeDriverSetUp();
+        openHomePage();
+        setUpObjects();
+        maximizeWindow();
+        acceptCookies();
+    }
+
+}
---
+++ src/test/java/setup/SetUpFirefox.java
@@ -0,0 +1,14 @@
+package setup;
+import org.junit.Before;
+
+public class SetUpFirefox extends SetUpDriver {
+
+    @Before
+    public void fireFoxTestSetup() {
+        firefoxDriverSetUp();
+        openHomePage();
+        setUpObjects();
+        maximizeWindow();
+        acceptCookies();
+    }
+}
